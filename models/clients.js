4aaee27a4755142b42ee5e8eff713772b7ac3b1b
alvaro <alvaro89_piura@hotmail.com>
2023-02-27 12:01:27 -0500

    update of sql commands

--- models/clients.js
+++ models/clients.js
@@ -1,36 +1,41 @@
 const pool = require('../mysql/mysql_querys');
+
 const GET_CLIENTS = "SELECT cliente.idcliente, persona.razon_social, persona.nombre, persona.apellido,persona.fecha_cumpleannos, cliente.ultimo_pago, cliente.duracion_credit, cliente.saldo, cliente.credito_limite, cliente.esVip,persona.genero,cliente.calificacion_idcalificacion, calificacion.nombre AS calificacion FROM persona, cliente, calificacion WHERE persona.idpersona = cliente.persona_idpersona AND cliente.calificacion_idcalificacion = calificacion.idcalificacion;"
+const GET_CLIENTS_GENERAL = process.env.GET_CLIENTS_GENERAL
 const GET_CLIENT_BY_ID = "SELECT persona.nombre, persona.apellido, cliente.ultimo_pago, cliente.saldo, cliente.credito_limite FROM persona, cliente WHERE persona.idpersona = cliente.persona_idpersona AND cliente.idcliente = ?;"
-const GET_CLIENT_BY_NOMBRE = "";
-const GET_IDPERSONA = "SELECT persona_idpersona FROM cliente WHERE idcliente = ?"
+const GET_IDPERSONA = process.env.GET_IDPERSONA
 
-const UPDATE_NAME_PERSON = "UPDATE persona SET nombre = ? WHERE idpersona = (SELECT persona_idpersona FROM cliente WHERE idcliente = ?) ;"
-const UPDATE_TEL_PERSON = "UPDATE persona SET telefono = ? WHERE idpersona = (SELECT persona_idpersona FROM cliente WHERE idcliente = ?) ;"
-const UPDATE_LAST_NAME_PERSON = "UPDATE persona SET apellido = ? WHERE idpersona = (SELECT persona_idpersona FROM cliente WHERE idcliente = ?) ;"
+const UPDATE_NAME_PERSON = process.env.UPDATE_NAME_PERSON
+const UPDATE_TEL_PERSON = process.env.UPDATE_TEL_PERSON
+const UPDATE_LAST_NAME_PERSON = process.env.UPDATE_LAST_NAME_PERSON
+const UPDATE_RAZON_SOCIAL = process.env.UPDATE_RAZON_SOCIAL
+const UPDATE_EMAIL = process.env.UPDATE_EMAIL
+const UPDATE_SALDO = process.env.UPDATE_SALDO
 
-const CREATE_PERSON = "INSERT INTO persona (nombre,apellido,fecha_cumpleannos,fecha_creacion,telefono,razon_social,genero,esAdmin) VALUES (?,?,?,?,?,?,?,?)"
-const CREATE_CLIENT = "INSERT INTO cliente (idcliente,persona_idpersona,credito_limite,detalle,email,duracion_credit,esVip,estrellas) VALUES(?,?,?,?,?,?,?,?)"
-const DELETE_PERSON = "DELETE FROM persona WHERE idpersona = ? "
-const DELETE_CLIENT = "DELETE FROM cliente WHERE idcliente = ? "
-// TODO: Terminar de crear los metodos de UPDATE y testear DELETE
+const CREATE_PERSON = process.env.CREATE_PERSON
+const CREATE_CLIENT = process.env.CREATE_CLIENT
+
+const DELETE_PERSON = process.env.DELETE_PERSON
+const DELETE_CLIENT = process.env.DELETE_CLIENT
+
+// TODO: Terminar de crear los metodos de UPDATE 
+
 const Clients ={
     Cliente_idCliente : null,
     message : {error : false, status : 202, message : ''},
-    Create : async function (body) {
+    Create : async function (body = Object) {
         try {
             const {idcliente, nombre, apellido, fecha_cumpleannos, fecha_creacion, telefono, razon_social, credito_limite, email,genero, vip,duracion_credit, estrellas,detalle_cliente} = body;
             const id_persona = await pool.execute(CREATE_PERSON, [nombre || '', apellido || '', fecha_cumpleannos || '', fecha_creacion || '', telefono || 0, razon_social || 0, genero || '', 0]).then(([row,field])=>{
                 return row.insertId;
             })
-
-            await pool.execute(CREATE_CLIENT,[idcliente,id_persona, credito_limite||0,detalle_cliente||'',email||'',duracion_credit||'15d',vip||0,estrellas||5]).then((val)=>{
-                console.log(val[0].insertId);
-            })
-            return this.message;
+            await pool.execute(CREATE_CLIENT,[idcliente,id_persona, credito_limite||200, detalle_cliente||'',credito_limite || 200,email||'',duracion_credit||'30d', vip||0,estrellas||5]);
+            this.message.message = "Client Created Succesfully"
         } catch (error) {
-            console.log(new Error(error));
+            this.message.error = true
+            this.message.status = 500
+            this.message.message = error
         }
-        
     },
     GetClients : async function () {
         await pool.query(GET_CLIENTS)
@@ -42,6 +47,21 @@ const Clients ={
             this.message.status = 404
         });
         return this.message;
+    },
+    GetClientGeneral : async function () {
+        try {
+            await pool.query(GET_CLIENTS_GENERAL)
+            .then(res=>{
+                this.message.message = res[0]
+            }).catch(err=>{
+                this.message.error = true
+                this.message.message  = err
+                this.message.status = 404    
+            });
+            return this.message
+        } catch (error) {
+            
+        }
     },
     GetClientBytId : async function (idCliente) {
         await pool.query(GET_CLIENT_BY_ID, [idCliente]).then((row, fields)=>{
@@ -56,48 +76,114 @@ const Clients ={
         return this.message;
     },
     UpdateNombre : async function (idCliente = String , newNombre = String) {
-        await pool.execute(UPDATE_NAME_PERSON,[newNombre, idCliente ])
-        .then((el)=>{
-            this.message.message = `AffectedRows : ${el[0].affectedRows}`
-        }).catch((err)=>{
+        try {
+            await pool.execute(UPDATE_NAME_PERSON,[newNombre, idCliente])
+            .then((el)=>{
+                this.message.message = `Update Successfully, ${el[0].affectedRows}`
+            }).catch((err)=>{
+                this.message.error = true
+                this.message.message = err
+                this.message.status = 404
+            })
+        } catch (error) {
             this.message.error = true
-            this.message.message = err
+            this.message.message = error
             this.message.status = 404
-        })
-        return this.message;
+        }
     },
     UpdateApellido : async function (idCliente = String, newApellido = String) {
         try {
             await pool.execute(UPDATE_LAST_NAME_PERSON, [newApellido, idCliente])
-            return this.message
+            .then(res=>{
+                this.message.message = `Update Successfully, ${res[0].affectedRows}`
+            }).catch(err=>{
+                this.message.error = true
+                this.message.message = err
+                this.message.status = 404    
+            })
+
         } catch (error) {
-            return error
+            this.message.error = true
+            this.message.message = error
+            this.message.status = 404
         }
     },
     UpdateTelephone : async function (idCliente = String, tel = Number) {
-        await  pool.execute(UPDATE_TEL_PERSON, [tel, idCliente])
-        .then((val)=>{
-            this.message.message = `${val[0].info}`
-        }).catch((err)=>{
+        try {
+            await  pool.execute(UPDATE_TEL_PERSON, [tel, idCliente])
+            .then((val)=>{
+                this.message.message = `Update Successfully, ${val[0].info}`
+            }).catch((err)=>{
+                this.message.error = true
+                this.message.message = err
+                this.message.status = 404
+            });
+        } catch (error) {
             this.message.error = true
-            this.message.message = err
+            this.message.message = error
             this.message.status = 404
-        });
-        return this.message;
-
+        }
     },
-
+    UpdateEmail : async function (idCliente = String, newEmail = String) {
+        try {
+            await pool.execute(UPDATE_EMAIL, [newEmail, idCliente])
+            .then(res=>{
+                this.message.message = `Update Successfully, ${res[0].affectedRows}`
+            }).catch(err=>{
+                this.message.error = true
+                this.message.message = err
+                this.message.status = 404
+            })
+        } catch (error) {
+            this.message.error = true
+            this.message.message = error
+            this.message.status = 404            
+        }
+    },
+    UpdateRazonSocial : async function (idCliente = String, newRazonSocial = String) {
+        try {
+            await pool.execute(UPDATE_RAZON_SOCIAL,[newRazonSocial, idCliente]).then(res=>{
+                this.message.message = `Update Successfully, ${res[0].affectedRows}`
+            }).catch(err=>{
+                this.message.error = true
+                this.message.message = err
+                this.message.status = 404
+            })
+        } catch (error) {
+            this.message.error = true
+            this.message.message = error
+            this.message.status = 404 
+        }
+    },
+    UpdateSaldo : async function (idCliente = String, newSaldo = Number) {
+        try {
+            await pool.execute(UPDATE_SALDO,[newSaldo, idCliente])
+            .then(res=>{
+                this.message.message = `Updated Successfully, ${res[0].affectedRows}`
+            }).catch(err=>{
+                this.message.error = true
+                this.message.message = err
+                this.message.status = 404                
+            });
+        } catch (error) {
+            this.message.error = true
+            this.message.message = error
+            this.message.status = 404 
+        }
+    },
     DeleteClient : async function (idCliente =  String) {
         try {
             const persona_idpersona = await pool.query(GET_IDPERSONA, [idCliente]).then(([row,field])=>{
-                return row[0]
+                return row[0].persona_idpersona
             });
             await pool.execute(DELETE_PERSON,[persona_idpersona])
             await pool.execute(DELETE_CLIENT, [idCliente])
-    
-            return this.message
+
+            this.message.message = "Deleted Succesfully"
         } catch (error) {
-            return error            
+            this.message.error = true
+            this.message.message = error
+            this.message.status = 404
         }        
     }
 }
--- routes/clients.js
+++ routes/clients.js
@@ -1,5 +1,4 @@
 const routes = require('express').Router();
-const puppeteer = require('puppeteer');
 const Clients = require('../models/clients');
 const uuid = require('uuid');
 const jwt = require('jsonwebtoken');
@@ -10,7 +9,7 @@ routes.get('/', async (req, res, next)=>{
 });
 routes.get('/s/nombre/:name',async(req,res)=>{
     const q = req.params.name
-    const response = await Clients.GetClients();
+    const response = await Clients.GetClientGeneral();
     const result = response.message.filter((cliente)=>{
         const {nombre} = cliente
         return nombre.toUpperCase().includes(q.toUpperCase())
@@ -19,7 +18,7 @@ routes.get('/s/nombre/:name',async(req,res)=>{
 });
 routes.get('/s/apellido/:apellido',async(req,res)=>{
     const q = req.params.apellido
-    const response = await Clients.GetClients();
+    const response = await Clients.GetClientGeneral();
     const result = response.message.filter((cliente)=>{
         const {apellido} = cliente
         return apellido.toUpperCase().includes(q.toUpperCase())
@@ -45,22 +44,38 @@ routes.put('/',(req,res,next)=>{
 routes.put("/name/:name",async (req, res)=>{
     const name = req.params.name.toUpperCase()
     const idCliente = req.headers['client-id']
-    const result = await Clients.UpdateNombre(idCliente, name)
-    res.status(result.status).send(result.message);
+    await Clients.UpdateNombre(idCliente, name)
+    res.send(Clients.message)
 });
 
 routes.put("/apellido/:apellido",async (req,res)=>{
     const apellido = req.params.apellido.toUpperCase()
     const idCliente = req.headers['client-id']
-    const result = await Clients.UpdateApellido(idCliente, apellido);
-    res.send(result) 
-})
+    await Clients.UpdateApellido(idCliente, apellido);
+    res.send(Clients.message); 
+});
 
 routes.put("/tel/:telephone", async (req, res)=>{
     const tel = Number(req.params.telephone)
     const idCliente = req.headers['client-id']
-    const result = await Clients.UpdateTelephone(idCliente, tel)
-    res.status(result.status).send(result.message);    
+    await Clients.UpdateTelephone(idCliente, tel)
+    res.send(Clients.message);
+});
+
+// Usamos headers para que sean privados la información
+
+routes.put("/email",async (req,res)=>{
+    const idCliente = req.headers['client-id']
+    const email =  req.headers['client-email']
+    await Clients.UpdateEmail(idCliente, email);
+    res.send(Clients.message);
+});
+
+routes.put("/rznsc",async(req,res)=>{
+    const idCliente = req.headers['client-id']
+    const razon_social = req.headers['client-rznsc']
+    await Clients.UpdateRazonSocial(idCliente, razon_social)
+    res.send(Clients.message);
 })
 
 routes.post('/',(req,res, next)=>{
@@ -82,14 +97,24 @@ routes.post('/',async (req, res)=>{
     body.credito_limite = Number(body.credito_limite)
     body.nombre = body.nombre.toUpperCase();
     body.apellido = body.apellido.toUpperCase();
-    const result = await Clients.Create(body)
-    res.send(result)
+    await Clients.Create(body)
+    res.send(Clients.message)
+});
+
+routes.delete('/',async(req,res)=>{
+    const authHeader = req.headers["authorization"];
+    if (authHeader == null) return res.sendStatus(403);
+    jwt.verify(authHeader, process.env.TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(404);
+        console.log('token_correcto');
+        next();
+    });
 });
 
 routes.delete('/',async(req,res)=>{
     const idcliente = req.headers['client-id']
-    const response = await Clients.DeleteClient(idcliente)
-    res.send(response);
+    await Clients.DeleteClient(idcliente)
+    res.send(Clients.message)
 })
 
 
